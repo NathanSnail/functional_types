dadadad2b7d9142971437fd8f37e3da65c17c829
Nathan <47441022+NathanSnail@users.noreply.github.com>
2024-12-16 23:23:15 +1300

    split into seperate parser

---
+++ parser.py
@@ -0,0 +1,123 @@
+from dataclasses import dataclass
+
+from tokens import (Arrow, Backslash, Eq, Identifier, LiteralInt, LParen,
+                    RParen, Token, tokenise_src)
+
+PRETTY_LAMBDAS = True
+
+
+@dataclass
+class Value:
+    name: str
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return str(self)
+
+
+@dataclass
+class Lambda:
+    param_name: str
+    body: "Expression"
+
+    def __str__(self):
+        if PRETTY_LAMBDAS:
+            return f"(λ{self.param_name}.{self.body})"
+        else:
+            return f"(\\{self.param_name} -> {self.body})"
+
+    def __repr__(self):
+        # improper but idc
+        return str(self)
+
+
+@dataclass
+class Application:
+    fn: "Expression"
+    val: "Expression"
+
+    def __str__(self):
+        return f"{self.fn}({self.val})"
+
+    def __repr__(self):
+        return str(self)
+
+
+Expression = Value | Lambda | Application | LiteralInt
+
+
+@dataclass
+class Assignment:
+    name: str
+    val: Expression
+
+    def __str__(self):
+        return f"{self.name} = {self.val}"
+
+    def __repr__(self):
+        return str(self)
+
+
+def parse_value(toks: list[Token]) -> tuple[Expression, int]:
+    ptr = 0
+    match toks[ptr]:
+        case LParen():
+            ptr += 1
+            (expr, skip) = parse_statement(toks[ptr:])
+            ptr += skip
+            assert (
+                type(toks[ptr]) is RParen
+            ), f"Paren not closed, got {toks[ptr]} and had {toks}"
+            ptr += 1
+            return (expr, ptr)
+        case Backslash():
+            ptr += 1
+            param_name = toks[ptr]
+            assert (
+                type(param_name) is Identifier
+            ), f"Lambda param is not an identifier, got {param_name} and had {toks}"
+            ptr += 1
+            assert (
+                type(toks[ptr]) is Arrow
+            ), f"Lambda missing arrow, got {toks[ptr]} and had {toks}"
+            ptr += 1
+            (val, skip) = parse_statement(toks[ptr:])
+            ptr += skip
+            return (Lambda(param_name.name, val), ptr)
+        case LiteralInt(value):
+            return (LiteralInt(value), 1)
+        case Identifier(name):
+            return (Value(name), 1)
+    assert False, f"invalid token {toks[ptr]} for value, had {toks}"
+
+
+def parse_statement(toks: list[Token]) -> tuple[Expression, int]:
+    (built, ptr) = parse_value(toks)
+    while True:
+        if len(toks) == ptr or type(toks[ptr]) is RParen:
+            return (built, ptr)
+        (expr, skip) = parse_value(toks[ptr:])
+        built = Application(built, expr)
+        ptr += skip
+
+
+def parse_line(toks: list[Token]) -> Assignment:
+    id = toks[0]
+    assert (
+        type(id) is Identifier
+    ), f"Declaration name {id} is not an identifier for {toks}"
+    assert (
+        type(toks[1]) is Eq
+    ), f"Declaration does not have a equals, has {toks[1]} for {toks}"
+    (expr, skip) = parse_statement(toks[2:])
+    if len(toks[2:]) != skip:
+        assert (
+            False
+        ), f"Not all tokens consumed for line {toks}, had {toks[2+skip:]} remaining"
+    return Assignment(id.name, expr)
+
+
+def parse_src(src: str) -> list[Assignment]:
+    return [parse_line(x) for x in tokenise_src(src)]
--- tokens.py
+++ tokens.py
@@ -1,7 +1,5 @@
 from dataclasses import dataclass
 
-from main import LiteralInt
-
 
 @dataclass
 class Identifier:
@@ -45,3 +43,56 @@ class LiteralInt:
 
 
 Token = Eq | Arrow | Backslash | LParen | RParen | Identifier | LiteralInt
+
+def tokenise_line(src: str) -> list[Token]:
+    ptr = 0
+    build = ""
+    toks: list[Token] = []
+
+    def flush_cur():
+        nonlocal build
+        if build == "":
+            return
+        if (build[0] == "-" and build[1:].isdigit()) or build.isdigit():
+            toks.append(LiteralInt(int(build)))
+        else:
+            toks.append(Identifier(build))
+        build = ""
+
+    srclen = len(src)
+    while ptr < srclen:
+        cur = src[ptr]
+        match cur:
+            case "=":
+                flush_cur()
+                toks.append(Eq())
+            case "\\":
+                flush_cur()
+                toks.append(Backslash())
+            case "-":
+                flush_cur()
+                cur = "-"
+            case ">":
+                if cur != ">":
+                    assert (
+                        False
+                    ), f"Error when tokenising '{src}' > from -> without 1 - preceeding, had {cur}"
+                toks.append(Arrow())
+            case "(":
+                flush_cur()
+                toks.append(LParen())
+            case ")":
+                flush_cur()
+                toks.append(RParen())
+            case " ":
+                flush_cur()
+            case x:
+                build = build + x
+        ptr += 1
+    flush_cur()
+    return toks
+
+
+def tokenise_src(src: str) -> list[list[Token]]:
+    return [x for x in [tokenise_line(x) for x in src.split("\n")] if len(x) != 0]
+
